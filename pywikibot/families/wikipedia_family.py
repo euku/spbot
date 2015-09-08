28db3d26c5d6266f16f9c36bfd25a0ab01d50411
amir <ladsgroup@gmail.com>
2015-09-09 01:01:46 +0430

    Persian Wikipedia configuration in wikipedia_family.py
    - One of category redirect templates was wrong
    - /doc is translated in lots of cases
    
    Change-Id: I1cdf01086717771792e6023f6d3fc7e8aa4b9cdc
    (cherry picked from commit 056c53f42973cee5880bf744090628c925b90334)

--- pywikibot/families/wikipedia_family.py
+++ pywikibot/families/wikipedia_family.py
@@ -101,8 +101,7 @@ class Family(family.WikimediaFamily):
             'es': (u'Categoría redirigida',),
             'eu': (u'Kategoria redirect',),
             'fa': (u'رده بهتر',
-                   u'انتقال رده',
-                   u'فیلم‌های امریکایی',),
+                   u'انتقال رده',),
             'fr': (u'Redirection de catégorie',),
             'gv': (u'Aastiurey ronney',),
             'hi': (u'श्रेणीअनुप्रेषित',
@@ -548,7 +547,7 @@ class Family(family.WikimediaFamily):
         # TODO: Remove comments for appropriate pages
         self.doc_subpages = {
             '_default': ((u'/doc', ),
-                         ['ar', 'bn', 'cs', 'da', 'en', 'es', 'fa',
+                         ['ar', 'bn', 'cs', 'da', 'en', 'es',
                           'hu', 'id', 'ilo', 'ja', 'ms',
                           'ms', 'pt', 'ro', 'ru', 'simple', 'vi', 'zh']
                          ),
@@ -556,6 +555,7 @@ class Family(family.WikimediaFamily):
             'de': (u'Doku', u'/Meta'),
             'dsb': (u'/Dokumentacija', ),
             'eu': (u'txantiloi dokumentazioa', u'/dok'),
+            'fa': (u'/doc', u'/توضیحات'),
             # fi: no idea how to handle this type of subpage at :Metasivu:
             'fi': ((), ),
             'fr': (u'/documentation', ),
