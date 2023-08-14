e7785a22f2c25c9a7b169304b2505c624429beeb
Euku <euku@arcor.de>
2023-08-14 20:39:14 +0000

    switch to Kubernetes

--- jobs/daily-log-cleanup.sh
+++ jobs/daily-log-cleanup.sh
@@ -2,6 +2,7 @@
 cd /data/project/spbot/logs/
 today=`(date +%F)`
 mkdir "$today"
-mv ../archive-resolved-* ../bot-stats* ../daily-log-* ../pers-bek* ../mp-helper* "$today"
+mv archive-resolved-* bot-stats* daily-log-* pers-bek* mp-helper* "$today"
+
 bzip2 --compress "$today"/*
 rm "$today"/*.err "$today"/*.out
