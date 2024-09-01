3214ba49a44d16e874fc64e80c00b8dea2231c7d
xqt <info@gno.de>
2024-09-01 19:37:46 +0000

    use pywikibot.User to get additional informations; refactoring; new layout

--- scripts/userscripts/bot-stats.py
+++ scripts/userscripts/bot-stats.py
@@ -1,126 +1,200 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import sys              # To not have wikipedia and this in one dir we'll import sys
-import re               # Used for regular expressions
-import pywikibot        # pywikibot framework
-import operator
-import pywikibot.exceptions
-from pywikibot.data import api
-from pywikibot import textlib
-from datetime import datetime, date, timedelta
-from time import localtime, strftime, mktime    # strftime-Function and related
-import dateutil.parser
-import time
+"""Update bot statistic.
 
-botStatusInactiveThreshold = 3*356/12
-mysite = pywikibot.Site()  # ohne Parameter, falls de-wiki default-Einstellung ist
+The following parameters are supported:
 
-formerBotNames = ["ArchivBot", "LinkFA-Bot", "RevoBot", "MerlBot", "KLBot2", "Luckas-bot", "Sebbot", "Beitragszahlen", "CopperBot", "ZéroBot", "TXiKiBoT", "Thijs!bot", "MerlIwBot"]
+-always           The bot won't ask for confirmation when putting a page
 
-userDic = {}
-userDic["AkaBot"] = "2004-11-30"
-userDic["ApeBot"] = "2003-07-30"
-userDic["BWBot"] = "2004-08-10"
-userDic["Bota47"] = "2005-07-19"
-userDic["Botteler"] = "2004-08-23"
-userDic["Chlewbot"] = "2005-11-30"
-userDic["Chobot"] = "2005-06-18"
-userDic["ConBot"] = "2004-10-26"
-userDic["FlaBot"] = "2004-11-21"
-userDic["GeoBot"] = "2005-07-15"
-userDic["Gpvosbot"] = "2005-06-11"
-userDic["KocjoBot"] = "2005-10-08"
-userDic["LeonardoRob0t"] = "2004-11-30"
-userDic["MelancholieBot"] = "2005-09-22"
-userDic["PortalBot"] = "2005-11-01"
-userDic["PyBot"] = "2003-05-28"
-userDic["RKBot"] = "2005-04-10"
-userDic["RedBot"] = "2005-01-21"
-userDic["Robbot"] = "2003-10-11"
-userDic["RobotE"] = "2005-05-20"
-userDic["RobotQuistnix"] = "2005-07-17"
-userDic["Sk-Bot"] = "2004-10-20"
-userDic["SpBot"] = "2005-10-06"
-userDic["Tasca.bot"] = "2005-07-30"
-userDic["Tsca.bot"] = "2005-07-30"
-userDic["YurikBot"] = "2005-07-31"
-userDic["Zwobot"] = "2003-12-02"
+-summary:         Set the action summary message for the edit.
+"""
+#
+# (C) Euku, 2008-2024
+# (C) xqt, 2024
+#
+from __future__ import annotations
 
-def queryLastEdit(username):
-	# load user contribs from API
-        req = api.Request(site=mysite, action="query")
-        req['list'] = 'usercontribs'
-        req['ucuser'] = username
-        req['ucdir'] = 'older'
-        req['uclimit'] = 1
-        req['rawcontinue'] = ''
-        data = req.submit()
-        try:
-            for x in data['query']['usercontribs']:
-               return x['timestamp'] # last edit
-        except:
-            return None 
+from itertools import chain
+from operator import methodcaller
 
-if __name__ == "__main__":
-    formerBotList = []
-    req1 = api.Request(site=mysite, action="query")
-    req1['list'] = 'users'
-    req1['ususers'] = "|".join(str(x) for x in formerBotNames)
-    req1['usprop'] = 'editcount|registration'
-    data1 = req1.submit()
+import pywikibot  # pywikibot framework
+from pywikibot import Timestamp
+from pywikibot.backports import Generator, removeprefix
+from pywikibot.bot import CurrentPageBot, SingleSiteBot
+from pywikibot.tools.itertools import filter_unique
 
-    botList = []
-    req2 = api.Request(site=mysite, action="query")
-    req2['list'] = 'allusers'
-    req2['augroup'] = 'bot'
-    req2['aulimit'] = 'max'
-    req2['auprop'] = 'editcount|registration'
-    data2 = req2.submit()
-    for x in data1['query']['users'] + data2['query']['allusers']:
-        p1 = re.compile(r'(?P<date>\d\d\d\d\-\d\d\-\d\d).+', re.UNICODE)
-        matches1 = p1.finditer(x['registration'])
-        reg = "?"
-        for match1 in matches1:
-            reg = match1.group('date')
-        if reg == "?" or reg == datetime.today().strftime("%Y-%m-%d"):
-            if x['name'] in userDic:
-               reg = userDic[x['name']] + "*"
+# notable unflagged bots
+unflagged_bots = ['Beitragszahlen']
+# ignore users with (revoked) botflag
+ignore_users = [
+    'Geolina163 (WikiCon-Orga)',
+    'Martin Urbanec (WMF)',
+    'R. Hillgentleman',
+]
+
+
+class BotStatsUpdater(SingleSiteBot, CurrentPageBot):
+
+    """A bot which updates bot statistics."""
+
+    update_options = {'summary': 'Bot: Aktualisiere Bot-Statistik'}
+
+    def former_botnames(self) -> Generator[str, None, None]:
+        """Collect former botnames.
+
+        .. note:: This method yields account where bot flag was revoked.
+           It does not check whether the flag was granted afterwards.
+        """
+        pywikibot.info('find former bot names...', newline=False)
+        for cnt, event in enumerate(self.site.logevents('rights')):
+            if event.action() != 'rights':
+                continue
+            if 'bot' in event.oldgroups and 'bot' not in event.newgroups:
+                if cnt % 10 == 0:
+                    pywikibot.info('.', newline=False)
+                yield event.data['title']
+
+    @property
+    def generator(self) -> Generator[str, None, None]:
+        """Yield the page to update."""
+        yield pywikibot.Page(
+            self.site,
+            'Wikipedia:Liste der Bots nach Anzahl der Bearbeitungen'
+        )
+
+    def treat_page(self) -> None:
+        """Process the bot statistic page."""
+        page_header = (
+            'Aufgeführt sind alle Bots die einen Bot-Flag besitzen. '
+            'Stand: ~~~~~<br />'
+            'Ein Bot gilt als inaktiv, wenn er in den letzten drei Monaten '
+            'keinen Beitrag geleistet hat.\n\n'
+            '[//de.wikipedia.org/w/index.php?'
+            f'title={self.current_page.title(underscore=True)}'
+            '&diff=curr&oldid=prev&diffonly=1 Änderungen der letzten Woche]'
+        )
+
+        page_footer = (
+            '([[Wikipedia_Diskussion:'
+            'Liste der Bots nach Anzahl der Bearbeitungen'
+            '#Bots_ohne_einen_Edit_mit_einem_letzten_Edit|eine Schätzung]])'
+            '<br />\n<nowiki>*</nowiki> = Datum der ersten Bearbeitung<br/>\n'
+            'ehemalig = das Benutzterkonto besitzt kein Botflag mehr\n\n'
+            '[[Kategorie:Wikipedia:Bots]]'
+        )
+
+        table_header = """
+{|class="sortable wikitable"
+! #
+! Botname
+! Status
+! Beiträge
+! Gesamtbearbeitungen
+! Letzte Bearbeitung
+! Anmeldedatum
+
+"""
+
+        text = page_header + table_header + self.create_table() + page_footer
+        self.put_current(
+            text,
+            summary=self.opt.summary,
+            show_diff=not self.opt.always
+        )
+
+    def collect_data(self) -> Generator[pywikibot.User, None, None]:
+        """Collect flagged bots and bots with revoked flags."""
+        allbots = (user['name'] for user in self.site.allusers(group='bot'))
+        # Use bot users first in the chain.
+        # The bot flag can have been granted for former botnames
+        bots = (pywikibot.User(self.site, name)
+                for name in chain(allbots,
+                                  self.former_botnames(),
+                                  unflagged_bots))
+        container = {pywikibot.User(self.site, name)
+                     for name in ignore_users}
+        yield from filter_unique(bots, container=container)
+
+    def create_table(self) -> str:
+        """Create page content."""
+        botlist = sorted(self.collect_data(),
+                         key=methodcaller('editCount'),
+                         reverse=True)
+
+        pywikibot.info('\ncreating wiki table...', newline=False)
+        pagetext = ''
+        all_edits = 0
+        now = Timestamp.now()
+
+        for num, bot in enumerate(botlist, start=1):
+            if num % 10 == 0:
+                pywikibot.info('.', newline=False)
+
+            all_edits += bot.editCount()
+
+            last_edit = bot.last_edit[2] if bot.last_edit else None
+            if not last_edit:
+                last_edit_str = '-' if not bot.editCount() else '?'
             else:
-               reg = "?"
-        botList.append((textlib.replaceExcept(x['name'], "&amp;", "&", []), x['editcount'], reg))
-    
-    botList = reversed(sorted(botList, key=operator.itemgetter(1)))
-    pageText = '{{Benutzer:Euku/B:Navigation}}\n\
-Aufgeführt sind alle Bots die einen Bot-Flag besitzen. Stand: ~~~~~<br />Ein Bot gilt als inaktiv, wenn er in den letzten drei Monaten keinen Beitrag geleistet hat.\n\n\
-[//de.wikipedia.org/w/index.php?title=Benutzer:Euku/Botstatistik&diff=curr&oldid=prev&diffonly=1 &Auml;nderungen der letzten Woche]\n\
-{|class="sortable wikitable"\n\
-! #\n! Botname\n!Beiträge\n! Gesamtbearbeitungen\n! Letzte Bearbeitung\n! Anmeldedatum\n'
-    counter = 0
-    allEdits = 0
-    now = datetime.now()
-    for bot in botList:
-        counter += 1
-        botName, botEditCounter, botCreationDate = bot
-        allEdits += botEditCounter
-        remark = ""
-        lastEditRes = queryLastEdit(botName)
-        if (lastEditRes != None):
-            lastEdit = dateutil.parser.parse(lastEditRes).replace(tzinfo=None)
-            if botName in formerBotNames:
-                remark = "(ehemalig)"
-            elif (now - lastEdit).days > 3*30:
-                remark = "(inaktiv)"
-            pageText += "|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||%s||%s\n" % (counter, botName, botName, remark, botName, botEditCounter, lastEdit.strftime("%Y-%m-%d"), botCreationDate)
-        else:
-            if botName in formerBotNames:
-                remark = "(ehemalig)"
-            pageText += "|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||-||%s\n" % (counter, botName, botName, remark, botName, botEditCounter, botCreationDate)
-    
-    pageText += "|}\nGesamtbearbeitungen durch diese Bots: %s ([[Benutzer_Diskussion:Euku/Botstatistik#Bots_ohne_einen_Edit_mit_einem_letzten_Edit|eine Schätzung]])<br />\n<nowiki>*</nowiki> = Datum der ersten Bearbeitung<br/>\nehemalig = das Benutzterkonto besitzt kein Botflag mehr" % "{:,}".format(allEdits).replace(',', '.')
-    
-    # print pageText use pywikibot.out
-    # save it
-    pywikibot.output("Speichere...")
-    pageOp = pywikibot.Page(pywikibot.Site(), "Benutzer:Euku/Botstatistik")
-    pywikibot.output(pageText)
-    pageOp.put(pageText, "Update")
+                last_edit_str = str(last_edit.date())
+
+            reg = bot.registration()
+            if reg:
+                registration = str(reg.date())
+            else:
+                reg = bot.first_edit[2]
+                registration = f'{reg.date()} *' if reg else '?'
+
+            # for colors see https://meta.wikimedia.org/wiki/Brand/colours
+            remark = 'inaktiv'
+            if 'bot' not in bot.groups():
+                remark, color = 'ehemalig', 'E5C0C0'  # light red
+            elif not last_edit:
+                color = 'C0E6FF'  # light bright blue
+            elif (now - last_edit).days > 365:
+                color = 'FBDFC5'  # light orange
+            elif (now - last_edit).days > 182:
+                color = 'FBEEBF'  # light yellow
+            elif (now - last_edit).days > 91:
+                color = 'F9F9F0'  # light bright yellow
+            else:
+                remark, color = 'aktiv', 'DBF3EC'  # light bright green
+
+            edits = f'{bot.editCount():,}'.replace(',', '.')
+            pagetext += (
+                f'|-\n|{num}|'
+                f'|{bot.title(as_link=True, with_ns=False)}|'
+                f'| style="background:#{color}" |{remark}|'
+                f'|[[Spezial:Beiträge/{bot.title(with_ns=False)}|B]]|'
+                f'|{edits}|'
+                f'|{last_edit_str}|'
+                f'|{registration}\n'
+            )
+            self.counter['collect'] = num
+
+        pywikibot.info()
+        edit_sum = f'{all_edits:,}'.replace(',', '.')
+        pagetext += f'|}}\nGesamtbearbeitungen durch diese Bots: {edit_sum} '
+        return pagetext
+
+
+def main(*args: str) -> None:
+    """Process command line arguments and invoke bot.
+
+    If args is an empty list, sys.argv is used.
+
+    :param args: command line arguments
+    """
+    options = {}
+
+    for arg in pywikibot.handle_args():
+        opt, _, value = arg.partition(':')
+        if not opt.startswith('-'):
+            continue
+        options[removeprefix(opt, '-')] = value or True
+
+    bot = BotStatsUpdater(**options)
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
